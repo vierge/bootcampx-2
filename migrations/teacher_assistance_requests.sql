8af1f263dedbb8feafd12404cbbba6c1be6de39f
vierge <marancholia@gmail.com>
2020-04-29 18:06:34 +0000

    added ERD-2 diagram and teacher_assistance_requests migrations

---
+++ migrations/teacher_assistance_requests.sql
@@ -0,0 +1,23 @@
+-- DROP TABLE any existing tables to avoid conflicts or duplicates
+DROP TABLE assistance_requests;
+DROP TABLE teachers;
+
+CREATE TABLE teachers(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  is_active BOOLEAN DEFAULT TRUE,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE assistance_requests(
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  student_feedback TEXT,
+  teacher_feedback TEXT
+);
